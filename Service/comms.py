fd13d76de47b41b5371ab10160de84a90d29aed2
Emiliano <112063@tecnicatura.frc.utn.edu.ar>
2022-11-15 23:50:22 -0300

    20221115-Se crea el modúlo "Service" el cúal esta destinado a ser el servicio que manejará los datos y acceso a txts, se trabaja con Flask. **A trabajar**: en búsqueda de la mejor manera de importar los nuevos modúlos creados por uno mismo, debido a esto en el modúlo "routes", el .py routes contiene funciones que no hacen a la obtención de una ruta, entonces estas funciones deberían estar contempladas en otro archivo.

---
+++ Service/comms.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from routes import routes as Routes
+
+_service = Flask(__name__)
+
+
+@_service.route(Routes.get_a_specific_route(route_name = "WRITE_LINE"))
+def write_line(text_to_write = ''):
+    
+    try:
+        
+        ret = {
+            'response': 'El texto fue escrito con éxito!'
+        }
+        
+        return jsonify(ret)
+        
+        
+    except Exception as e:
+        
+        pass   
+
+if __name__.__eq__('__main__'):
+    _service.run()
---
+++ Service/test/test_comms.py
@@ -0,0 +1,33 @@
+import json
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+
+
+def test():
+    
+    try:
+        
+        default_endpoint = '/writeline'
+        
+        url = 'http://localhost:5000{}'.format(default_endpoint)
+        response = urlopen(url)
+        data = json.loads(response.read())
+        
+        print(data)
+        
+    except URLError as e:
+        
+        #lg
+        pass
+    except HTTPError as e:
+        
+        #lg
+        pass
+    except Exception as e:
+        
+        #lg
+        pass
+
+if __name__.__eq__('__main__'):
+    
+    test()
