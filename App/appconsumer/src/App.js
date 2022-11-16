2066b9125142ec47d178e9c58d3aea2012de85a2
Emiliano <112063@tecnicatura.frc.utn.edu.ar>
2022-11-16 01:12:49 -0300

    20221116-Se crea módulo "App" el cual contiene la aplicación en ReactJS, esta es la que consume el WebServices de Python que se aloja en módulo "Service". **Anotaciones**: Esta pensada como una aplicación sencilla, la cual tendra una interfaz grafica representando lo descripto en el documento. Se usa React.js, mientras se irá actualizando el archivo README.md con el fin de otorgar instrucciones para una amigable ejecución de ambos Servicios/Módulos

---
+++ App/appconsumer/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Aquí se desarrollará el App Consumer
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
