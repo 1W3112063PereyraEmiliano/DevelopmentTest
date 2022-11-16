537f52764178e5d435c55574c06bff46304283ee
Emiliano <112063@tecnicatura.frc.utn.edu.ar>
2022-11-16 18:22:23 -0300

    20221116-Se agregan alertas de bootstrap al llamar al Service. Se realizan llamadas de tipo async y await. **Cosas a tener en cuenta** Me gustaría ver de trabajar solamente con await y no con .then del post. Ver el tema de la promesa que realiza el new Promise.

--- App/appconsumer/src/App.js
+++ App/appconsumer/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import { Badge, Col, Container, Row, Button, Form, Stack } from 'react-bootstrap'
+import { Badge, Col, Container, Row, Button, Form, Stack, Alert } from 'react-bootstrap'
 import { FiSend } from 'react-icons/fi'
 import { GiConfirmed, GiCancel } from 'react-icons/gi'
 import axios from 'axios';
@@ -8,26 +8,41 @@ import axios from 'axios';
 function App() {
 
   const [text, setText] = useState('');
+  const [dataFromResult, setDataFromResult] = useState()
+  const [show, setShow] = useState(false)
 
   const handleChange = event => {
     setText(event.target.value);
   };
 
-  const writeInFile = () => {
+  const writeInFile = async () => {
 
-    const textToWrite = { 
+    const textToWrite = {
       'text': text
     }
 
-    axios.post('writeline',textToWrite).then((response) => {
-        console.log(response)
-      }).catch((error) => {
-        if (error.response) {
-          console.log(error.response)
-          console.log(error.response.status)
-          console.log(error.response.headers)
-        }
-      })
+    const result = {}
+
+    const res = await axios.post('writeline', textToWrite).then((response) => {
+      result.message = response.data.response
+      result.status = response.status
+    }).catch((error) => {
+      if (error.response) {
+        result.message = error.response.data.response
+        result.status = error.response.status
+      }
+    })
+
+    setDataFromResult(result)
+    setShow(true)
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        setShow(false)
+        resolve();
+      }, 2000)
+    });
+
   }
 
   return (
@@ -46,16 +61,56 @@ function App() {
               <Stack gap={2} className="col-md-5 mx-auto">
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                   <Form.Label>Type text to write in .txt</Form.Label>
-                  <Form.Control value={text} onChange={handleChange} as="textarea" rows={4} />
+                  <Form.Control maxLength={256} value={text} onChange={handleChange} as="textarea" rows={4} />
                 </Form.Group>
                 <Button onClick={writeInFile} variant="secondary"> <FiSend></FiSend> Submit</Button>
               </Stack>
             </Col>
           </Row>
+          {
+            dataFromResult && (
+              <>
+                {
+                  dataFromResult.status === 200 && (
+                    <>
+                      <Row>
+                        <Col md="12">
+                          <Stack gap={2} className="col-md-5 mx-auto mt-1">
+                            <Alert show={show} key={'success'} variant={'success'}>
+                              <h6>
+                                {dataFromResult.message}
+                              </h6>
+                            </Alert>
+                          </Stack>
+                        </Col>
+                      </Row>
+                    </>
+                  )
+                }
+                {
+                  dataFromResult.status === 500 && (
+                    <>
+                      <Row>
+                        <Col md="12">
+                          <Stack gap={2} className="col-md-5 mx-auto mt-1">
+                            <Alert show={show} key={'danger'} variant={'danger'}>
+                              <h6>
+                                {dataFromResult.message}
+                              </h6>
+                            </Alert>
+                          </Stack>
+                        </Col>
+                      </Row>
+                    </>
+                  )
+                }
+              </>
+            )
+          }
         </Container>
 
       </header>
-    </div>
+    </div >
   );
 }
 
