0d111a84a9eb2354f1c1c0def0b84049d3f736fd
Emiliano <112063@tecnicatura.frc.utn.edu.ar>
2022-11-16 11:24:38 -0300

    20221116-Modifico interfaz agregando nuevos componentes. Se agrega Bootstrap e icons. **Realizar npm i**

--- App/appconsumer/src/App.js
+++ App/appconsumer/src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
+import {Badge, Col, Container, Row, Button, Form, Stack} from 'react-bootstrap'
+import {FiSend} from 'react-icons/fi'
+import {GiConfirmed, GiCancel} from 'react-icons/gi'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Aquí se desarrollará el App Consumer
-        </p>
-        
+        <h3>
+          App Consumer <Button variant="dark">
+            Connected <Badge bg="success"> <GiConfirmed></GiConfirmed> </Badge>
+            <span className="visually-hidden">unread messages</span>
+          </Button>
+        </h3>
+
+        <Container>
+          <Row>
+            <Col md="12">
+              <Stack gap={2} className="col-md-5 mx-auto">
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Type text to write in .txt</Form.Label>
+                  <Form.Control as="textarea" rows={4} />
+                </Form.Group>
+                <Button variant="secondary"> <FiSend></FiSend> Submit</Button>
+              </Stack>
+            </Col>
+          </Row>
+        </Container>
+
       </header>
     </div>
   );
